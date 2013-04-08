048dc016752d15e82c5372e6137d067393bd3297
Tim Oxley <secoif@gmail.com>
2013-04-08 02:05:28 -0400

    Refactor and tidy selector code.

--- index.js
+++ index.js
@@ -1,14 +1,126 @@
-var classes = require('classes')
-var matches = require('matches-selector')
+"use strict"
 
 var Emitter = require('emitter')
+var classes = require('classes')
+var matchesSelector = require('matches-selector')
+
+var DEFAULT_SELECTOR = 'body *'
+var DEFAULT_INVALID_SELECTOR = ''
+var DEFAULT_HIGHLIGHTED_CLASS = 'highlighted'
+var DEFAULT_SELECTED_CLASS = 'selected'
+var DEFAULT_SELECT_EVENT = 'click'
+var DEFAULT_PREFIX_CLASS = 'element-selector'
+var DEFAULT_ROOT_ELEMENT = document.body
+
+var DEFAULT_ENABLED = false
+var DEFAULT_USE_STYLES = true
+
+function ElementSelector(options) {
+  if (!(this instanceof ElementSelector)) return new ElementSelector(options)
+
+  options = options || {}
+  this.selector = options.selector || DEFAULT_SELECTOR
+  this.invalidSelector = options.invalidSelector || DEFAULT_INVALID_SELECTOR
+
+  this.highlightedClass = options.highlightedClass || DEFAULT_HIGHLIGHTED_CLASS
+
+  this.selectedClass = options.selectedClass || DEFAULT_SELECTED_CLASS
+
+  this.selectEvent = options.selectEvent || DEFAULT_SELECT_EVENT
+
+  this.prefixClass = options.prefixClass || DEFAULT_PREFIX_CLASS
+
+  this.useDefaultStyles = options.useDefaultStyles || DEFAULT_USE_STYLES
+
+  this.root = options.root || DEFAULT_ROOT_ELEMENT
+
+  this.enabled = options.enabled != null // Likely a Boolean
+    ? !!options.enabled
+    : DEFAULT_ENABLED
+
+  this.highlighted = undefined
+
+  bus.on('mouseover', this.highlight.bind(this))
+  bus.on('mouseout', this.dehighlight.bind(this))
+  bus.on(this.selectEvent, this.select.bind(this))
+
+  this.enabled ? this.enable() : this.disable()
+}
+
+
+ElementSelector.prototype.matches = function matches(el) {
+  return !! el && matchesSelector(el, this.selector) &&
+         !(this.invalidSelector && matchesSelector(el, this.invalidSelector))
+}
+
+ElementSelector.prototype.disable = function disable() {
+  this.enabled = false
+  this.deselect()
+  return this
+}
+
+ElementSelector.prototype.enable = function enable() {
+  this.enabled = true
+  if (!this.useDefaultStyles) return this
+  classes(this.root).add(this.prefixClass)
+  return this
+}
+
+ElementSelector.prototype.highlight = function highlight(el, e) {
+  if (!this.enabled) return this
+  if (this.matches(el)) {
+    if (this.highlighted && this.highlighted !== el) this.dehighlight(this.highlighted)
+    classes(el).add(this.highlightedClass)
+    this.highlighted = el
+    this.emit('highlight', el, e)
+  }
+  return this
+}
+
+ElementSelector.prototype.dehighlight = function dehighlight(el, e) {
+  if (!this.enabled) return this
+  el = el || this.highlighted
+  if (this.matches(el)) {
+    classes(el).remove(this.highlightedClass)
+    this.highlighted = null
+    this.emit('dehighlight', el, e)
+  }
+  return this
+}
+
+ElementSelector.prototype.select = function select(el, e) {
+  if (!this.enabled) return this
+  if (this.selected) this.deselect(null, e)
+  if (this.matches(el)) {
+    this.dehighlight()
+    classes(el).add(this.selectedClass)
+    this.selected = el
+    this.emit('select', el, e)
+  }
+}
+
+ElementSelector.prototype.deselect = function deselect(el, e) {
+  if (!this.enabled) return this
+  el = el || this.selected
+  if (this.matches(el)) {
+    this.dehighlight()
+    classes(el).remove(this.selectedClass)
+    this.selected = null
+    this.emit('deselect', el, e)
+  }
+  return this
+}
+
+Emitter(ElementSelector.prototype)
+
+/**
+ * Global bus for listening to mouse events.
+ */
+
 var bus = new Emitter()
 
-var initialized = false
-
-var initialize = function() {
-  if (initialized) return
-  initialized = true
+function initializeEvents() {
+  if (initializeEvents.complete) return
   document.addEventListener('mouseover', function(e) {
     bus.emit('mouseover', e.target, e)
   })
@@ -27,112 +139,10 @@ var initialize = function() {
   document.addEventListener('dblclick', function(e) {
     bus.emit('dblclick', e.target, e)
   })
-}()
 
-module.exports = function(options) {
-  return new ElementSelector(options)
+  initializeEvents.complete = true
 }
 
-module.exports.ElementSelector = ElementSelector
+initializeEvents()
 
-function ElementSelector(options) {
-  options = options || {}
-  this.selector = options.selector || 'body *'
-  this.invalidSelector = options.invalidSelector || ''
-
-  this.highlightedClass = options.hasOwnProperty('highlightedClass')
-    ? options.highlightedClass || null
-    : 'highlighted'
-  this.selectedClass = options.hasOwnProperty('selectedClass')
-    ? options.selectedClass || null
-    : 'selected'
-
-  this.selectEvent = options.hasOwnProperty('selectEvent')
-    ? options.selectEvent || null
-    : 'click'
-
-  this.enabled = typeof options.enabled === "undefined"
-    ? options.enabled
-    : false
-
-  this.enable()
-
-  this.highlighted = null
-  this.highlight = highlight.bind(this)
-  this.dehighlight = dehighlight.bind(this)
-  this.select = select.bind(this)
-  this.deselect = deselect.bind(this)
-  this.disable = disable.bind(this)
-  this.enable = enable.bind(this)
-  this.matches = elMatches.bind(this)
-
-  bus.on('mouseover', this.highlight)
-  bus.on('mouseout', this.dehighlight)
-  bus.on(this.selectEvent, this.select)
-}
-
-ElementSelector.prototype = {}
-
-function disable() {
-  this.deselect()
-  this.enabled = false
-  return this
-}
-
-function enable() {
-  if (!this.enabled) return false
-  if (!this.useDefaultStyles) return
-  classes(document.body).add('element-selector')
-  return this
-}
-
-function elMatches(el) {
-  return !! el && matches(el, this.selector) &&
-      !(this.invalidSelector && matches(el, this.invalidSelector))
-}
-
-function highlight(el, e) {
-  if (!this.enabled) return
-  if (this.matches(el)) {
-    if (this.highlighted && this.highlighted !== el) this.dehighlight(this.highlighted)
-    classes(el).add(this.highlightedClass)
-    this.highlighted = el
-    this.emit('highlight', el, e)
-  }
-}
-
-function dehighlight(el, e) {
-  if (!this.enabled) return
-  el = el || this.highlighted
-  if (this.matches(el)) {
-    classes(el).remove(this.highlightedClass)
-    this.highlighted = null
-    this.emit('dehighlight', el, e)
-  }
-}
-
-function select(el, e) {
-  if (!this.enabled) return
-  if (this.selected) this.deselect(null, e)
-  if (this.matches(el)) {
-    this.dehighlight()
-    classes(el).add(this.selectedClass)
-    this.selected = el
-    this.emit('select', el, e)
-  }
-}
-
-function deselect(el, e) {
-  if (!this.enabled) return
-  el = el || this.selected
-  if (this.matches(el)) {
-    this.dehighlight()
-    classes(el).remove(this.selectedClass)
-    this.selected = null
-    this.emit('deselect', el, e)
-  }
-}
-
-
-Emitter(ElementSelector.prototype)
-
+module.exports = ElementSelector
